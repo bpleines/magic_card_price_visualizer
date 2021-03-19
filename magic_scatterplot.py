e3a6927484e7bfc609cf24893589c0a7871e94a3
Branden Pleines <bpleines@Brandens-MacBook-Pro.local>
2021-03-19 12:00:00 -0700

    iterating

--- magic_scatterplot.py
+++ magic_scatterplot.py
@@ -3,6 +3,8 @@ import requests
 import os.path
 from os import path
 
+mtg_set_codes = ['STX', 'KHM', 'ZNR', 'IKO', 'THB', 'ELD', 'WAR', 'RNA', 'GRN', 'DOM', 'RIX', 'XLN', 'HOU', 'AKH', 'AER', 'KLD', 'EMN', 'SOI', 'OGW', 'BFZ', 'DTK', 'FRF', 'KTK', 'JOU', 'BNG']
+
 def encode_color(color):
    # Handle colorless case
    if len(color) == 0:
@@ -17,7 +19,6 @@ def encode_color(color):
        "B": "#000000",
        "G": "#087500"
      }
-     print(color[0]) 
      return color_map[color[0]]
 
 def get_card_info(set_code):
@@ -26,45 +27,33 @@ def get_card_info(set_code):
   cards = []
   for card in response:
     card_dict = {}
-    card_dict["name"] = card.get("name")
-    card_dict["cmc"] = int(card.get("cmc"))
-    card_dict["price"] = float(card.get("prices").get("usd"))
-    card_dict["colors"] = card.get("color_identity")
-    cards.append(card_dict)
+    try:
+      card_dict["name"] = card.get("name")
+      card_dict["cmc"] = int(card.get("cmc"))
+      card_dict["price"] = float(card.get("prices").get("usd"))
+      card_dict["colors"] = card.get("color_identity")
+      cards.append(card_dict)
+    except TypeError:
+      print("The card " + card.get("name") + " was missing an expected value. Skipping!")
+      continue
   return cards
 
-'''
-def generate_scatterplot(set_code='ZNR'):
-  sorted_cards_by_price = sorted(get_card_info(set_code), key = lambda i: i['price'])
-  fig, ax = plt.subplots()
-  for color in ['red', 'white', 'green', 'blue', 'black']:
-    on_color_cards = [ card for card in sorted_cards_by_price if encode_color(color) in card["colors"] ]
-    x = [ card['cmc'] for card in on_color_cards ]
-    y = [ card['price'] for card in on_color_cards ] 
-    scale = 100.0
-    ax.scatter(x, y, c=color, s=scale, label=color,
-               alpha=0.4, edgecolors='black')
-
-  ax.set_xlabel('Converted Mana Cost (cmc)', fontsize=15)
-  ax.set_ylabel('Price of Card ($)', fontsize=15)
-  ax.set_title('Magic the Gathering Rares (' + set_code + ') : Price by Mana Cost and Color')
-  ax.legend()
-  ax.grid(True)
-  plt.show()
-'''
-
-# Some other interesting codes: RAV, 2XM, IKO
-# generate_scatterplot('CHK')
-def generate_card_csv(csv_file_path='/Users/bpleines/dataVisualization/finalProject/data_vis/magiccards.csv'):
-  cards = get_card_info('ZNR')
+def generate_card_csv(set_code='ZNR'):
+  csv_file_path= '/Users/bpleines/dataVisualization/finalProject/data_vis/' + set_code + '.csv'
+  cards = get_card_info(set_code)
   if os.path.exists(csv_file_path):
     os.remove(csv_file_path)  
   with open(csv_file_path, 'a') as mycsv:
     mycsv.write('cmc,price,color\n')
     for card in cards:
       mycsv.write(str(card["cmc"]) + "," + str(card["price"]) + ',' + str(encode_color(card["colors"])) + '\n' )
+  print("Generated csv data for set: " + set_code)
+
+def git_commit_and_push():
   os.system('git add *')
   os.system('git commit -m iterating')
   os.system('git push')
 
-generate_card_csv()
+for set_code in mtg_set_codes:
+  generate_card_csv(set_code)
+git_commit_and_push()
