fc8fd99112e669c66509307fa785dc73399264ca
Branden Pleines <branden@firebolt.io>
2022-12-12 14:34:40 -0800

    test new scanning

--- color_magic_scryfall_data_scraper.py
+++ color_magic_scryfall_data_scraper.py
@@ -25,7 +25,9 @@ def pandas_implementation(color_code='R'):
             'release_year': [card["release_year"] for card in cards],
             'price': [card["price"] for card in cards],
             'colors': [card["colors"] for card in cards],
-            'image_uri': [card["image_uri"] for card in cards]
+            'image_uri': [card["image_uri"] for card in cards],
+            'type_line': [card["type_line"] for card in cards],
+            'artist': [card["artist"] for card in cards]
         }
     )
     return cards_data_frame
@@ -40,7 +42,9 @@ def extract_card_details(card_list):
           card_dict["cmc"] = int(card.get("cmc"))
           card_dict["release_year"] = int(card.get("released_at").split('-')[0])
           card_dict["price"] = float(card.get("prices").get("usd"))
-          card_dict["colors"] = card.get("color_identity")
+          card_dict["colors"] = MTGCodes().encode_color(card.get("color_identity"))
+          card_dict["type_line"] = card.get("type_line")
+          card_dict["artist"] = card.get("artist")
           try:
               card_dict["image_uri"] = card.get("image_uris").get("normal")
           except AttributeError as e:
@@ -51,32 +55,13 @@ def extract_card_details(card_list):
           continue
   return cards
 
-def get_card_info(color_code):
-    cards = []
-    url = f"https://api.scryfall.com/cards/search?q=color%3D{color_code}%28rarity%3Ar+OR+rarity%3Am%29"
-    response = requests.get(url).json()
-    data = response.get("data")
-    for card in extract_card_details(data):
-        cards.append(card)
-    while response.get('has_more'):
-        response = requests.get(response.get('next_page')).json()
-        for card in extract_card_details(response.get('data')):
-            cards.append(card)
-    return cards
-
 def generate_card_csv(color_code='W'):
-    for color_code in MTGCodes().get_color_codes():
-        csv_file_path = f"{pathlib.Path(__file__).parent.absolute()}/magic_card_csv_files_by_color/{color_code}.csv"
-        cards = get_card_info(color_code)
-        print(cards)
-        if os.path.exists(csv_file_path):
-            os.remove(csv_file_path)
-        with open(csv_file_path, 'a') as mycsv:
-            mycsv.write('name,cmc,release_year,price,color,image\n')
-            for card in cards:
-                print(card)
-                mycsv.write(f"{card['name']},{card['cmc']},{card['release_year']},{card['price']},{MTGCodes().encode_color(card['colors'])},{card['image_uri']}\n")
-        print(f"Generated csv data for set: {color_code}")
+    csv_file_path = f"{pathlib.Path(__file__).parent.absolute()}/magic_card_csv_files_by_color/{color_code}.csv"
+    cards = pandas_implementation(color_code)
+    if os.path.exists(csv_file_path):
+        os.remove(csv_file_path)
+    cards.to_csv(csv_file_path)
+    print(f"Generated csv data for set: {color_code}")
 
 for color_code in MTGCodes().get_color_codes():
-    pandas_implementation(color_code)
+    generate_card_csv(color_code)
