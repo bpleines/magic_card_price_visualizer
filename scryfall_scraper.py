09d8a8776c902a63cbb5cde0bbcb11224ec7f42a
Branden Pleines <13591727+bpleines@users.noreply.github.com>
2023-05-04 16:57:06 -0700

    chore: improve tests and syntax (#30)
    * chore: improve tests and syntax
    
    * chore: improve tests and syntax
    
    * chore: improve tests and syntax
    
    * chore: improve tests and syntax

--- scryfall_scraper.py
+++ scryfall_scraper.py
@@ -6,6 +6,7 @@ import requests
 
 from codes import MTGCodes
 
+
 class ScryfallScraper:
     def __init__(self):
         self.mtg_color_codes = MTGCodes().get_color_codes()
@@ -25,11 +26,15 @@ class ScryfallScraper:
                 card_attributes = {}
                 try:
                     # Replace the commas in name to adhere to csv delimeter
-                    card_attributes["name"] = card.get("name").replace(',', '-')
+                    card_attributes["name"] = card.get("name").replace(",", "-")
                     card_attributes["cmc"] = int(card.get("cmc"))
-                    card_attributes["release_year"] = int(card.get("released_at").split('-')[0])
+                    card_attributes["release_year"] = int(
+                        card.get("released_at").split("-")[0]
+                    )
                     card_attributes["price"] = float(card.get("prices").get("usd"))
-                    card_attributes["color"] = MTGCodes().encode_color(card.get("color_identity"))
+                    card_attributes["color"] = MTGCodes().encode_color(
+                        card.get("color_identity")
+                    )
                     card_attributes["type_line"] = card.get("type_line")
                     card_attributes["artist"] = card.get("artist")
                     # If the card is missing image_uris, give a harcoded image uri matching its color
@@ -37,30 +42,44 @@ class ScryfallScraper:
                         card_attributes["image"] = card.get("image_uris").get("normal")
                     except AttributeError as exc:
                         logging.debug(exc)
-                        card_attributes["image"] = MTGCodes().encode_default_image_for_color(card_attributes["color"])
+                        card_attributes[
+                            "image"
+                        ] = MTGCodes().encode_default_image_for_color(
+                            card_attributes["color"]
+                        )
                     cards.append(card_attributes)
                 except TypeError:
-                    logging.debug(f"The card {card.get('name')} was missing an expected value. Skipping!")
+                    logging.debug(
+                        f"The card {card.get('name')} was missing an expected value. Skipping!"
+                    )
                     continue
         return cards
 
-    def generate_local_csv(self, set_or_color_code : str):
+    def generate_local_csv(self, set_or_color_code: str):
         base_path = pathlib.Path(__file__).parent.absolute()
         if set_or_color_code in self.mtg_color_codes:
-            csv_file_path = f"{base_path}/magic_card_csv_files_by_color/{set_or_color_code}.csv"
+            csv_file_path = (
+                f"{base_path}/magic_card_csv_files_by_color/{set_or_color_code}.csv"
+            )
             card_data = self.query_card_color_data(set_or_color_code)
         else:
-            csv_file_path = f"{base_path}/magic_card_csv_files_by_set/{set_or_color_code}.csv"
+            csv_file_path = (
+                f"{base_path}/magic_card_csv_files_by_set/{set_or_color_code}.csv"
+            )
             card_data = self.query_card_set_data(set_or_color_code)
         cards = self.extract_card_data(card_data)
         if os.path.exists(csv_file_path):
             os.remove(csv_file_path)
         if cards is not None:
-            with open(csv_file_path, 'a', encoding="utf-8") as local_csv_file:
-                local_csv_file.write('name,cmc,release_year,price,color,image,type_line,artist\n')
+            with open(csv_file_path, "a", encoding="utf-8") as local_csv_file:
+                local_csv_file.write(
+                    "name,cmc,release_year,price,color,image,type_line,artist\n"
+                )
                 for card in cards:
                     # TODO: break out into multiple lines
-                    local_csv_file.write(f"{card['name']},{card['cmc']},{card['release_year']},{card['price']},{card['color']},{card['image']},{card['type_line']},{card['artist']}\n")
+                    local_csv_file.write(
+                        f"{card['name']},{card['cmc']},{card['release_year']},{card['price']},{card['color']},{card['image']},{card['type_line']},{card['artist']}\n"
+                    )
             logging.info(f"Completed csv data generation for: {set_or_color_code}")
 
     def populate_all_local_csv(self):
@@ -72,18 +91,14 @@ class ScryfallScraper:
             logging.info(f"Generating csv data for {mtg_color_code}")
             self.generate_local_csv(mtg_color_code)
 
-    def query_card_color_data(self, color_code : str):
+    def query_card_color_data(self, color_code: str):
         url = f"https://api.scryfall.com/cards/search?q=color%3D{color_code}%28rarity%3Ar+OR+rarity%3Am%29"
-        response = requests.get(
-            url,
-            timeout=self.requests_timeout_seconds
-        ).json()
+        response = requests.get(url, timeout=self.requests_timeout_seconds).json()
         card_data = [response.get("data")]
         page_count = 1
-        while response.get('has_more'):
+        while response.get("has_more"):
             response = requests.get(
-                response.get('next_page'),
-                timeout=self.requests_timeout_seconds
+                response.get("next_page"), timeout=self.requests_timeout_seconds
             ).json()
             card_data.append(response.get("data"))
             logging.info(f"Appended page {page_count} for color {color_code}")
@@ -94,12 +109,9 @@ class ScryfallScraper:
             return None
         return card_data
 
-    def query_card_set_data(self, set_code : str):
+    def query_card_set_data(self, set_code: str):
         url = f"https://api.scryfall.com/cards/search?q=set%3A{set_code}+%28rarity%3Ar+OR+rarity%3Am%29"
-        response = requests.get(
-            url,
-            timeout=self.requests_timeout_seconds
-        ).json()
+        response = requests.get(url, timeout=self.requests_timeout_seconds).json()
         # Making card_data a single element list is unnecessary but matches paginated color design
         # This allows extract_card_data to be identical for each
         card_data = [response.get("data")]
--- test_scryfall_scraper.py
+++ test_scryfall_scraper.py
@@ -2,30 +2,40 @@ import unittest
 
 from scryfall_scraper import ScryfallScraper
 
-class TestScryfallScraper(unittest.TestCase):
 
+class TestScryfallScraper(unittest.TestCase):
     scraper = ScryfallScraper()
 
     def test_extract_card_data(self):
-        card_data = self.scraper.query_card_set_data('CHK')
+        card_data = self.scraper.query_card_set_data("CHK")
         cards = self.scraper.extract_card_data(card_data)
         single_card = cards[0]
-        self.assertTrue(single_card["name"] is not None)
-        self.assertTrue(single_card["cmc"] is not None)
-        self.assertTrue(single_card["release_year"] is not None)
-        self.assertTrue(single_card["color"] is not None)
-        self.assertTrue(single_card["type_line"] is not None)
-        self.assertTrue(single_card["artist"] is not None)
+        # This below tests on name confirm delimiter replacement for writing as csv file
+        self.assertFalse(
+            single_card["name"] == "Akki Lavarunner // Tok-Tok, Volcano Born"
+        )
+        self.assertTrue(
+            single_card["name"] == "Akki Lavarunner // Tok-Tok- Volcano Born"
+        )
+        self.assertTrue(single_card["cmc"] == 4)
+        self.assertTrue(single_card["release_year"] == 2004)
+        self.assertTrue(single_card["color"] == "#ff1a1a")
+        self.assertTrue(
+            single_card["type_line"]
+            == "Creature — Goblin Warrior // Legendary Creature — Goblin Shaman"
+        )
+        self.assertTrue(single_card["artist"] == "Matt Cavotta")
 
     def test_query_card_color_data(self):
-        card_data = self.scraper.query_card_color_data('W')
+        card_data = self.scraper.query_card_color_data("W")
         self.assertTrue(all(data is not None for data in card_data))
         self.assertTrue(len(card_data) > 1)
 
     def test_query_card_set_data(self):
-        card_data = self.scraper.query_card_set_data('CHK')
+        card_data = self.scraper.query_card_set_data("CHK")
         self.assertTrue(card_data[0] is not None)
         self.assertTrue(len(card_data) == 1)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
